e4172271944318ebf86be73c5ca4b68d71dd0277
Paulo Costa <pauloamcosta2@gmail.com>
2020-01-16 22:32:22 -0300

    Creating producers

--- src/main/java/com/pauloamcosta/ecommerce/NewOrderMain.java
+++ src/main/java/com/pauloamcosta/ecommerce/NewOrderMain.java
@@ -5,15 +5,21 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         var producer =  new KafkaProducer<String, String>(properties());
         var value = "12322, 67323, 78924343555";
         var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, value);
-        producer.send(record);
+        producer.send(record, (data, ex) -> {
+            if(ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("Success sending " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + " timestamp" + data.timestamp());
+        }).get();
     }
 
     private static Properties properties() {
